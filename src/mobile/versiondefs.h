1944b47ac32e3cfa47557793c84345c12cdfb203
Emile Belanger <emile.belanger@gmail.com>
2023-01-22 14:51:36 +0000

    Fix version string

--- src/mobile/versiondefs.h
+++ src/mobile/versiondefs.h
@@ -5,7 +5,7 @@
 #define BINNAME "ecwolf"
 #define GAME_DIR "ecwolf"
 
-#define DOTVERSIONSTR_NOREV "1.3.99999"
+#define DOTVERSIONSTR_NOREV "1.4.0"
 #define SAVEPRODVER 1148831 // 0xMMMmmmrr in hex
 
 // Windows RC files have weird syntax so we need an unquoted version
